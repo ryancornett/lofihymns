e6389c2aa3127b41b0cf8be06f355c841b1db15d
Ryan Cornett <ryandcornett@gmail.com>
2024-06-07 10:59:20 -0400

    add link styling and observers

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
-const hero = document.querySelector('.hero');
-hero.addEventListener('click', function() {
-    hero.classList.toggle('grayscale-100');
-});
+let options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.1
+  };
+  
+  let slideUpToVisible = function(entries, observer) {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            if(!entry.target.classList.contains('observed')) {
+                entry.target.classList.add('observed');            
+            }
+            observer.unobserve(entry.target);
+        }
+    });
+  };
+  
+  let observer = new IntersectionObserver(slideUpToVisible, options);
+  
+  let targets = document.querySelectorAll('.observable');
+  targets.forEach(target => {
+    observer.observe(target);
+  });
